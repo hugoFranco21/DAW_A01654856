b5b5b5336528a8ed0888d1bfcc07547a9261ea9b
hugoTec <hfranco21@hotmail.com>
2019-01-29 20:58:19 -0600

    terminar lab 4

--- Lab4/scripts.js
+++ Lab4/scripts.js
@@ -1,17 +1,35 @@
 /*Javascript file for lab 4*/
+var indiceTab = 1;
+var indicePl = 1;
+
 function func1() {
   var num = prompt("Ingresa un numero");
   var table = document.createElement('table');
-  //var currentRow;
-  //var element;
-  for(var i = 1; i <= num; i++){
-    table.insertRow();
+  for(var i = 0, k = 0; i <= num; i++, k++){
+    var row = document.createElement('tr');
     for(var j = 0; j < 3; j++){
       var item = document.createElement('td');
-      item.innerHTML = i*Math.pow(i,j);
-      console.log(i*Math.pow(i,j));
+      if(i == 0){
+        switch(j){
+          case 0:
+            var textnode = document.createTextNode('Numero');
+            break;
+          case 1:
+            var textnode = document.createTextNode('Cuadrado');
+            break;
+          case 2:
+            var textnode = document.createTextNode('Cubo');
+            break;
+        }
+      } else{
+        var textnode = document.createTextNode((i*Math.pow(i,j)).toString());
+      }
+      item.appendChild(textnode);
+      row.appendChild(item);
     }
+    table.appendChild(row);
   }
+  document.getElementById("fun1").appendChild(table);
 }
 
 function func2() {
@@ -19,16 +37,23 @@ function func2() {
   var x = Math.floor(Math.random() * 100);
   var y = Math.floor(Math.random() * 100);
   var res = x+y;
-  console.log(x);
-  console.log(y);
+  //console.log(x);
+  //console.log(y);
   var num = prompt("Ingresa el resultado de " + x + " + " + y);
   endTime = new Date();
   var timeDiff = endTime - startTime;
   if(num == res){
     alert('Correcto, tardaste ' + timeDiff/1000 + ' segundos en escribir tu respuesta');
+    var par = document.createElement('p');
+    var tex = document.createTextNode('Acierto, tiempo: ' + timeDiff/1000 + ' segundos.');
+    par.appendChild(tex);
   } else{
     alert('Incorrecto, la respuesta correcta era ' + res +', tardaste ' + timeDiff/1000 + ' segundos en escribir tu respuesta');
+    var par = document.createElement('p');
+    var tex = document.createTextNode('Error, tiempo: ' + timeDiff/1000 + ' segundos.');
+    par.appendChild(tex);
   }
+  document.getElementById("fun2").appendChild(par);
 }
 
 function func3(){
@@ -46,25 +71,48 @@ function func3(){
     } else if(arreglo[i] == 0){
       cer.push(arreglo[i]);
     } else{
-      pos.push(arreglo[i])
+      pos.push(arreglo[i]);
     }
   }
-  imprime_Ar(neg);
-  imprime_Ar(cer);
-  imprime_Ar(pos);
-  console.log(neg.length);
-  console.log(cer.length);
-  console.log(pos.length);
-}
+  var tab = document.createElement('table');
+  var row = document.createElement('tr');
+  var cell = document.createElement('th');
+  //var par1 = document.createElement('p');
+  var tex = document.createTextNode(arreglo);
+  cell.appendChild(tex);
+  row.appendChild(cell);
+  tab.appendChild(row);
+  var tex = document.createTextNode('Hay ' + neg.length + ' numeros negativos en el arreglo');
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.appendChild(tex);
+  row.appendChild(cell);
+  tab.appendChild(row);
+  //document.getElementById('fun3').appendChild(par);
+  //console.log(cer.length);
+  var tex = document.createTextNode('Hay ' + cer.length + ' ceros en el arreglo');
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.appendChild(tex);
+  row.appendChild(cell);
+  tab.appendChild(row);
+  //document.getElementById('fun3').appendChild(par);
+  //console.log(pos.length);
+  var tex = document.createTextNode('Hay ' + pos.length + ' numeros positivos en el arreglo');
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.appendChild(tex);
+  row.appendChild(cell);
+  tab.appendChild(row);
+  document.getElementById('fun3').appendChild(tab);
 
-function imprime_Ar(params){
-  for(var i = 0; i < params.length; i++){
-    console.log(params[i] + ' ');
-  }
 }
 
 function func4(){
   var n = prompt('Ingresa el numero de elementos de una fila de la matriz');
+  var tabl = document.createElement('table');
+  var proms = document.getElementById('resp4');
+  var ind = 1;
   var arreglo = new Array(n);
   var salida = new Array(n);
   for(var i = 0; i < n; i++){
@@ -72,12 +120,25 @@ function func4(){
   }
   for(var i = 0; i < n; i++){
     salida[i] = 0;
+    var row = document.createElement('tr');
+    var rr = proms.insertRow(ind);
+    ind++;
     for(var j = 0; j < n; j++){
+      var cell = document.createElement('td');
       arreglo[i][j] = Math.floor(Math.random() * 100) - 50;
+      var tex = document.createTextNode(arreglo[i][j].toString());
       salida[i] += arreglo[i][j];
+      cell.appendChild(tex);
+      row.appendChild(cell);
     }
+    tabl.appendChild(row);
     salida[i] /= n;
+    var cell1 = rr.insertCell(0)
+    var cell2 = rr.insertCell(1);
+    cell1.innerHTML = (i+1).toString();
+    cell2.innerHTML = (salida[i]).toString();
   }
+  document.getElementById('fun4').appendChild(tabl);
   console.log(arreglo);
   console.log(salida);
 }
@@ -86,13 +147,20 @@ function func5(){
   var n = prompt('Ingresa el numero que quieres invertir');
   var x;
   x = parseInt(n.toString().split('').reverse().join(''));
-  console.log(x);
+  var table = document.getElementById('inver');
+  var row = table.insertRow(indicePl);
+  indicePl++;
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = n.toString();
+  cell2.innerHTML = x.toString();
 }
 
 function func6(){
-  var num, count = 0, au = 10, ans, aux, aux2, aux3;
+  var num, count = 0, au = 10, ans, aux, aux2, aux3, aux4;
   //int num, count = 0, au = 10, ans, aux, aux2, aux3;
   num = prompt('Ingresa el balance de cuenta actual');
+  aux4 = num;
 	//scanf("%i", &num);
 	if(num >= 0){
 		ans = num;
@@ -117,5 +185,14 @@ function func6(){
 			}
 		}
 	}
-  console.log("resp =" + ans);
+  if(num != null){
+    var table = document.getElementById('probACM');
+    var row = table.insertRow(indiceTab);
+    indiceTab++;
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = aux4.toString();
+    cell2.innerHTML = ans.toString();
+  }
+  //console.log("resp =" + ans);
 }
