80c15374c32372e5a33045be083de3c0f7f8b2ff
hugoTec <hfranco21@hotmail.com>
2019-02-07 00:33:55 -0600

    finalizar tutorial fonts

--- lab6/js/tutor.js
+++ lab6/js/tutor.js
@@ -2,6 +2,12 @@
 var pika = document.getElementById('pikachu');
 var i = 0;
 var posiciones = ['static','relative','fixed', 'absolute', 'sticky'];
+var cam = document.getElementById('cambiar');
+var fuentes = ['sans-serif','serif','Monospace'];
+var est = ['normal','italic','oblique'];
+var tama = ['xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'];
+
+
 
 pika.onclick = function modificar_pos(){
   //pika.style.position(posiciones[i % 5]);
@@ -9,4 +15,43 @@ pika.onclick = function modificar_pos(){
   //pika.style.position = 'sticky';
   console.log(pika.style.position);
   i++;
+}
+
+document.getElementById('familia').onclick = function fam(){
+  cam.style.fontFamily = fuentes[i % 3];
+  i++;
+  //console.log(cam.style.fontFamily);
+}
+
+document.getElementById('estilo').onclick = function es(){
+  cam.style.fontStyle = est[i % 3];
+  i++;
+  //console.log(cam.style.fontStyle);
+}
+
+document.getElementById('tam').onclick = function fsize(){
+  cam.style.fontSize = tama[i % 7];
+  i++;
+  //console.log(cam.style.fontSize);
+}
+
+document.getElementById('neg').onclick = function fneg(){
+  if(cam.style.fontWeight == 400){
+    cam.style.fontWeight = 700;
+    document.getElementById('neg').innerHTML = 'Desactivar negritas';
+  } else{
+    cam.style.fontWeight = 400;
+    document.getElementById('neg').innerHTML = 'Activar negritas';
+  }
+}
+
+document.getElementById('inv').onclick = function finv(){
+  if(cam.style.visibility != 'hidden'){
+    cam.style.visibility = 'hidden';
+    document.getElementById('inv').innerHTML = 'Hacer visible';
+  } else{
+    cam.style.visibility = 'visible';
+    document.getElementById('inv').innerHTML = 'Hacer invisible';
+  }
+  //console.log(cam.style.fontSize);
 }
