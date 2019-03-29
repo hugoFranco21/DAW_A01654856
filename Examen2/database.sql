98fc7bebc51c854eeae811225cd80c39b19bd196
hugoTec <hfranco21@hotmail.com>
2019-03-29 13:07:25 -0600

    subir archivos examen

--- Examen2/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Fruit(
-  id_fruit int NOT NULL AUTO_INCREMENT,
-  name varchar(40),
-  units numeric(4,0),
-  quantity numeric(4,0),
-  price numeric(5,2),
-  country varchar(40),
-  CONSTRAINT pk_idfruit PRIMARY KEY (id_fruit)
-)
--- Examen2/index.php
+++ Examen2/index.php
@@ -1,17 +1,9 @@
 <?php
-  require_once('utils.php');
-  require_once('util.php');
-  _header();
-  _forma('Insertar registro','fruit.php');
-  _forma('Actualizar registro','updateController.php');
-  _smallForm('Borrar registro','deleteController.php');
-  consul();
-  echo '<h3 class="text-white">Toda la tabla fruits</h3>';
-  imprime_consulta(getFruits());
-  echo '<br/>
-        </div>';
-  _preguntas();
-  _referencias();
-  _footer();
+include("views/_header.html");
 
+include("views/_tablaIncidentes.html");
+
+include("views/_formaRegistroIncidente.html");
+
+include("views/_footer.html");
 ?>
