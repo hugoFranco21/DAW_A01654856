ce14201d134b3bdb8a8fed097269fdd84e271587
hugoTec <hfranco21@hotmail.com>
2019-03-30 16:39:52 -0600

    empezar lab19

---
+++ Lab19/index.php
@@ -0,0 +1,17 @@
+<?php
+  require_once('utils.php');
+  require_once('util.php');
+  _header();
+  _forma('Insertar registro','fruit.php');
+  _forma('Actualizar registro','updateController.php');
+  _smallForm('Borrar registro','deleteController.php');
+  consul();
+  echo '<h3 class="text-white">Toda la tabla fruits</h3>';
+  imprime_consulta(getFruits());
+  echo '<br/>
+        </div>';
+  _preguntas();
+  _referencias();
+  _footer();
+
+?>
